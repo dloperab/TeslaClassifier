ce3f0f55f0c80e6a1574030d5f37813d4b617561
David Lopera <davidlopera@outlook.com>
2019-05-20 08:40:41 -0500

    upload dataset

--- scripts/dataset/build_dataset.py
+++ scripts/dataset/build_dataset.py
@@ -2,15 +2,35 @@
 from search_bing_api import SearchBingApi
 
 # Set global params
-TRAIN_MAX_RESULTS = 60
-TRAIN_GROUP_SIZE = 60
-TEST_MAX_RESULTS = 20
-TEST_GROUP_SIZE = 20
+MAX_RESULTS = 120
+GROUP_SIZE = 40
 
 # Roadster
 query = "tesla roadster"
-output = "../../dataset/train/roadster"
+output = "../../dataset/teslas/train/roadster"
 print("[INFO] Downloading images for Tesla Roadster")
-SearchBingApi.search(query, output, TRAIN_MAX_RESULTS, TRAIN_GROUP_SIZE)
-output = "../../dataset/test/roadster"
-SearchBingApi.search(query, output, TEST_MAX_RESULTS, TEST_GROUP_SIZE)
+SearchBingApi.search(query, output, MAX_RESULTS, GROUP_SIZE)
+
+# Tesla Model 3
+query = "tesla model 3"
+output = "../../dataset/teslas/train/model_3"
+print("[INFO] Downloading images for Tesla Model 3")
+SearchBingApi.search(query, output, MAX_RESULTS, GROUP_SIZE)
+
+# Tesla Model S
+query = "tesla model s"
+output = "../../dataset/teslas/train/model_s"
+print("[INFO] Downloading images for Tesla Model S")
+SearchBingApi.search(query, output, MAX_RESULTS, GROUP_SIZE)
+
+# Tesla Model X
+query = "tesla model x"
+output = "../../dataset/teslas/train/model_x"
+print("[INFO] Downloading images for Tesla Model X")
+SearchBingApi.search(query, output, MAX_RESULTS, GROUP_SIZE)
+
+# Tesla Semi
+query = "tesla semi"
+output = "../../dataset/teslas/train/semi"
+print("[INFO] Downloading images for TTesla Semi")
+SearchBingApi.search(query, output, MAX_RESULTS, GROUP_SIZE)
